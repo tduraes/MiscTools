592e41858a4b971d28144ad16b0a073b9dd194da
TGD9 <duraes.tiago@gmail.com>
2023-04-28 00:26:09 +0100

    using this to test a game component

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import './styles.css';
 import PasswordGenerator from './PasswordGenerator';
 import PasswordValidator from './PasswordValidator';
 import TimeTracker from './TimeTracker'
+import Game from './Jumper';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <TimeTracker />
-        <PasswordGenerator />
-        <PasswordValidator />
+        <Game />
+        {/*<TimeTracker />*/}
+        {/*<PasswordGenerator />*/}
+        {/*<PasswordValidator />*/}
   </React.StrictMode>
 );
 
